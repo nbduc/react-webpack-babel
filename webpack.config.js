c975868dcbcac727b1498940a21bed672bdc6abb
Nguyen Ba Duc <1760286@student.hcmus.edu.vn>
2021-02-03 17:32:09 +0700

    Add Async functions

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: [
+    "./src/index.js", 
+    './node_modules/regenerator-runtime/runtime.js',
+  ],
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js"
